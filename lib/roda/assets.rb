0496506a4fcfa77f60b0bbc521433119c66723f0
cj <cjlazell@gmail.com>
2015-11-23 16:19:50 -0600

    initial commit

---
+++ lib/roda/assets.rb
@@ -0,0 +1,7 @@
+require "roda/assets/version"
+
+module Roda
+  module Assets
+    # Your code goes here...
+  end
+end
---
+++ lib/roda/assets/version.rb
@@ -0,0 +1,5 @@
+module Roda
+  module Assets
+    VERSION = "0.0.1"
+  end
+end
